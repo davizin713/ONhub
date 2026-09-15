181ab308d35a43dd35f7e3ee7235f9e836f23796
davizin713 <davi.allbano@yahoo.com>
2026-09-15 16:46:05 -0300

    Update script URL in main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandauth.com/virtual/file/9c1391edb95e44b2"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e9a718c4aa67afa3cc466239affd6d4da2b07b8154f7d006668a4aaf114c4932/download"))()
